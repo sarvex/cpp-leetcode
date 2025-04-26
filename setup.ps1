80fcb1ebc3aa44fcf67678f02d6718e4a5e50ae5
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2025-04-27 04:39:47 +0530

    Add 3000+ LeetCode solutions in C++

---
+++ setup.ps1
@@ -0,0 +1,20 @@
+# Remove all files except JS and TS files
+Get-ChildItem -Path . -Recurse -File | Where-Object { $_.Extension -notin ".cpp" } | Remove-Item -Force
+
+# Function to process code files
+function Process-Files($extension) {
+    Get-ChildItem -Path . -Recurse -Filter "*$extension" -File | ForEach-Object {
+        $parentDirName = Split-Path (Split-Path $_.Directory -Parent) -Leaf
+        $targetDir = Split-Path (Split-Path (Split-Path $_.Directory -Parent) -Parent) -Parent
+        $newPath = Join-Path $targetDir "$parentDirName.$extension"
+
+        if (-not (Test-Path $newPath)) {
+            Move-Item $_.FullName $newPath
+        }
+    }
+}
+
+# Process both file types
+Process-Files ".cpp"
+
+Write-Output "Setup completed successfully!"
