52e234037644bd2213d50704dd5858aecfe01eb4
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2025-04-27 08:52:46 +0530

    0001.Two Sum.cpp

--- 0001.Two Sum.cpp
+++ 0001.Two Sum.cpp
@@ -1,14 +1,17 @@
+#include <unordered_map>
+#include <vector>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> d;
-        for (int i = 0;; ++i) {
-            int x = nums[i];
-            int y = target - x;
-            if (d.contains(y)) {
-                return {d[y], i};
-            }
-            d[x] = i;
-        }
+  std::vector<int> twoSum(std::vector<int> &nums, int target) {
+    std::unordered_map<int, int> d;
+    for (int i = 0;; ++i) {
+      int x = nums[i];
+      int y = target - x;
+      if (d.contains(y)) {
+        return {d[y], i};
+      }
+      d[x] = i;
     }
+  }
 };
