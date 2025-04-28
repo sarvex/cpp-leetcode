082f397ed26d60bfba416ccd60b295df740b27b4
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2025-04-28 10:32:39 +0530

    Updated NVim

--- 0003.Longest Substring Without Repeating Characters.cpp
+++ 0003.Longest Substring Without Repeating Characters.cpp
@@ -1,15 +1,17 @@
+#include <string>
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int cnt[128]{};
-        int ans = 0, n = s.size();
-        for (int l = 0, r = 0; r < n; ++r) {
-            ++cnt[s[r]];
-            while (cnt[s[r]] > 1) {
-                --cnt[s[l++]];
-            }
-            ans = max(ans, r - l + 1);
-        }
-        return ans;
+  int lengthOfLongestSubstring(std::string s) {
+    int cnt[128]{};
+    int ans = 0, n = s.size();
+    for (int l = 0, r = 0; r < n; ++r) {
+      ++cnt[s[r]];
+      while (cnt[s[r]] > 1) {
+        --cnt[s[l++]];
+      }
+      ans = std::max(ans, r - l + 1);
     }
+    return ans;
+  }
 };
--- 0007.Reverse Integer.cpp
+++ 0007.Reverse Integer.cpp
@@ -1,13 +1,16 @@
+#include <limits>
+
 class Solution {
 public:
-    int reverse(int x) {
-        int ans = 0;
-        for (; x; x /= 10) {
-            if (ans < INT_MIN / 10 || ans > INT_MAX / 10) {
-                return 0;
-            }
-            ans = ans * 10 + x % 10;
-        }
-        return ans;
+  int reverse(int x) {
+    int ans = 0;
+    for (; x; x /= 10) {
+      if (ans < std::numeric_limits<int>::min() / 10 ||
+          ans > std::numeric_limits<int>::max() / 10) {
+        return 0;
+      }
+      ans = ans * 10 + x % 10;
     }
+    return ans;
+  }
 };
